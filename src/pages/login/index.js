432af50c698d4804b41f47d4054e96ecebc6a634
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-11-26 16:12:54 +0800

    toast notifications

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <h4>Login</h4>
+
+      <span>Don't have an account?</span>
+      <Link to='/register'>Register</Link>
+    </div>
+    
   )
 }
 
 export default Login
+
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,4 +1,9 @@
 import {useState} from 'react'
+import { Link } from 'react-router-dom';
+import '../../App.css';
+import './register.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -6,7 +11,9 @@ function Register() {
 
     const [user, setUser] = useState({
         userName: "",
-        password: ""
+        email: "",
+        password: "",
+        confirmPassword: ""
     });
     
     const handleRegisterInput = (e) => {
@@ -18,19 +25,61 @@ function Register() {
       
     };
 
-    const registerUser = () => {
+    const handleUserValidation = (e) => {
+        e.preventDefault()
+        if (user.password === user.confirmPassword) {
+            console.log(user)
+            toast.success('Account created!', {
+                position: "top-right",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+            
+            // async function createUser() {
+            //     try {
+            //     //   let resp = await axios.post("", {
+            //     //     userName: user.userName,
+            //     //     email: user.email,
+            //     //     password: user.password
+            //     //   });
+            //     //   console.log(resp.data)
+            //     //   console.log(resp)
+            //     }
+            //     catch (err) {
+            //       console.error(err);
+            //     }
+            //   }
+            
+        } else {
+            toast.warn('passwords must match!', {
+                position: "top-right",
+                autoClose: 4000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });
+            
+        }
+    };
 
-    }
       
 
 
   return (
-    <div>
+    <div className="container-register">
         
 
         <h3>register</h3>
 
-        <form onSubmit={registerUser} className="container-register-form">
+        <form onSubmit={handleUserValidation} className="container-register-form">
             <input 
             type="text" 
             placeholder="name" 
@@ -40,17 +89,39 @@ function Register() {
             />
 
             <input 
+            type="email" 
+            placeholder="email" 
+            value={user.email}
+            name="email" 
+            onChange={handleRegisterInput}
+            />
+
+            <input 
             type="password" 
             placeholder="password" 
             value={user.password} 
             name="password" 
-            onChange={handleRegisterInput}/>
+            onChange={handleRegisterInput}
+            />
+
+            <input 
+            type="password" 
+            placeholder="confirm password" 
+            value={user.confirmPassword} 
+            name="confirmPassword" 
+            onChange={handleRegisterInput}
+            />
 
             <input type="submit" value="Register"/>
         </form>
 
+        <span>Already have an account?</span>
+        <Link to='/login'>Login</Link>
+
+        <ToastContainer />
+
+
     </div>
   )
 }
-
 export default Register
