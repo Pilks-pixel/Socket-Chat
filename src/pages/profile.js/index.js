98eae2add9e7c971d96ea83e755212eac8c76405
Pilks-pixel <peterpilkington@hotmail.com>
2023-06-07 15:00:13 +0800

    modal refactor

--- src/pages/profile.js/index.js
+++ src/pages/profile.js/index.js
@@ -1,20 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../../App.css";
 import "../register/register.css";
 import { Avatar } from "../../components";
+import axios from "axios";
 
 function Profile() {
 	let location = useLocation();
 	const [newAvatar, setNewAvatar] = useState("");
 	const [displaySettings, setDisplaySettings] = useState(false);
+	const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
+	const ref = useRef();
 
 	const settingsToggle = () =>
 		setDisplaySettings(prevSettings => !prevSettings);
 
 	const deleteUser = () => {
-		// modal with display none
-	}	
+		console.log("User delete");
+		// axios.
+	};
+
+	// Modal Logic
+	const closeModal = e => {
+		if (e.target.className !== "btn--yes" && e.target.className !== "btn") {
+			dialog.close();
+			setDisplayDeleteModal(prevDeleteModal => !prevDeleteModal);
+		}
+	};
+
+	const dialog = ref.current;
+	useEffect(() => {
+		if (displayDeleteModal) {
+			dialog.showModal();
+			window.addEventListener("click", closeModal);
+
+			return () => {
+				dialog.close();
+				window.removeEventListener("click", closeModal);
+			};
+		}
+	}, [displayDeleteModal]);
 
 	return (
 		<div className='container'>
@@ -35,13 +60,24 @@ function Profile() {
 			{displaySettings && (
 				<Avatar selectedAvatar={newAvatar} setSelectedAvatar={setNewAvatar} />
 			)}
-			<div className="modal_container"></div>
-				<button>Delete Account</button>
-				<div className="modal">
-					<p>Are you sure you wish to delete your account?</p>
-					<button className="yes">Yes</button>
-					<button className="no">no</button>
-				</div>
+
+			<button
+				className='btn'
+				onClick={() =>
+					setDisplayDeleteModal(prevDeleteModal => !prevDeleteModal)
+				}
+			>
+				Delete Account
+			</button>
+
+			<dialog ref={ref} className='modal' aria-modal='true' role="alertdialog">
+				<h3>Are you sure you wish to delete your account?</h3>
+				<button className='btn--no'>no</button>
+				<button onClick={deleteUser} className='btn--yes'>
+					Yes
+				</button>
+			</dialog>
+
 			<div>
 				<span>Go to </span>
 				<Link to='/'>Chat</Link>
